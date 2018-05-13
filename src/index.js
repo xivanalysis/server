13b63b8ebf8980789e7ab39e676b089ef6327b63
Saxon Landers <saxon@ackwell.com.au>
2018-05-14 00:14:39 +1000

    Added handling for static public file serving

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import 'dotenv/config'
+import 'dotenv/config' // NOTE: This will need changing if we split this file.
 import Koa from 'koa'
 import Router from 'koa-router'
+import serve from 'koa-static'
 
 const app = new Koa()
-const router = new Router()
 
-router.get('/', ctx => {
-	ctx.body = 'This was a triumph!'
+// Serve the main public assets
+// TODO: Would be nice to be able to hit a url without the /#/
+app.use(serve(process.env.PUBLIC_PATH || 'public'))
+
+// Set up the router for the API
+const router = new Router()
+router.get('/api', ctx => {
+	ctx.body = 'TODO: Like, the entire API.'
 })
 
 app.use(router.routes())
 app.use(router.allowedMethods())
+
+// Boot the server
 app.listen(process.env.PORT || 3001)
