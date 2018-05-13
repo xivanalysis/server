f4549cab725d6d6b16db9fd5838203979ed933ca
Saxon Landers <saxon@ackwell.com.au>
2018-05-13 21:11:40 +1000

    Added dotenv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import Koa from 'koa'
 
 const app = new Koa()
@@ -6,4 +7,4 @@ app.use(ctx => {
 	ctx.body = 'This was a triumph!'
 })
 
-app.listen(7564)
+app.listen(process.env.PORT || 3001)
