8a186101a9f44077ae83feafb2aefb70081655cc
Saxon Landers <saxon@ackwell.com.au>
2018-05-13 23:58:42 +1000

    Added koa-router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import 'dotenv/config'
 import Koa from 'koa'
+import Router from 'koa-router'
 
 const app = new Koa()
+const router = new Router()
 
-app.use(ctx => {
+router.get('/', ctx => {
 	ctx.body = 'This was a triumph!'
 })
 
+app.use(router.routes())
+app.use(router.allowedMethods())
 app.listen(process.env.PORT || 3001)
