d862b0c2308ca0bd11f28ec6d2cf415137927b59
Saxon Landers <saxon@punchbuggy.com.au>
2018-05-14 11:52:58 +1000

    Added initial redis impl

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import Koa from 'koa'
+import Redis from 'ioredis'
 
 import compress from 'koa-compress'
 import Router from 'koa-router'
 import serve from 'koa-static'
 
 const app = new Koa()
+const redis = new Redis(process.env.REDIS_URL)
 
 // Shrink that stuff up a bit
 app.use(compress())
@@ -16,8 +18,8 @@ app.use(serve(process.env.PUBLIC_PATH || 'public'))
 
 // Set up the router for the API
 const router = new Router()
-router.get('/api', ctx => {
-	ctx.body = 'TODO: Like, the entire API.'
+router.get('/api', async ctx => {
+	ctx.body = await redis.get('test')
 })
 
 app.use(router.routes())
