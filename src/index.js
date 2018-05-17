f77d8057c6256a58bf34be75925144efa64a2192
Saxon Landers <saxon@ackwell.com.au>
2018-05-17 23:13:32 +1000

    Added initial fflogs proxy

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import 'dotenv/config'
+import axios from 'axios'
 import Koa from 'koa'
 import Redis from 'ioredis'
 
 import compress from 'koa-compress'
+import mount from 'koa-mount'
 import Router from 'koa-router'
 import serve from 'koa-static'
 
@@ -24,6 +26,41 @@ router.get('/api', async ctx => {
 
 app.use(router.routes())
 app.use(router.allowedMethods())
+
+// FFLogs proxy
+const fflogsApi = axios.create({
+	baseURL: 'https://www.fflogs.com/v1/',
+	params: {
+		api_key: process.env.FFLOGS_API_KEY
+	}
+})
+
+app.use(mount('/proxy/fflogs', async ctx => {
+	// Build the URL that we'll be requesting (and using as a key!)
+	const url = ctx.request.url
+
+	// Check if we have a cached copy, return that if we do
+	const key = 'proxy:fflogs:' + url
+	if (await redis.exists(key)) {
+		ctx.body = await redis.get(key)
+		return
+	}
+
+	// No cached copy, grab it down
+	let response = null
+	try {
+		response = await fflogsApi.get(url)
+	} catch(e) {
+		// TODO: Handle this better.
+		ctx.body = JSON.stringify([e.response.status, e.response.data])
+		return
+	}
+
+	// Save and respond
+	const data = response.data
+	redis.set(key, JSON.stringify(data))
+	ctx.body = data
+}))
 
 // Boot the server
 app.listen(process.env.PORT || 3001)
