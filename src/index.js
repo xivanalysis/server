e0cad51a5835e491b43b0509d4d8e39da2042066
Saxon Landers <saxon@ackwell.com.au>
2018-05-27 21:25:49 +1000

    Server changes for BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import Koa from 'koa'
 import compress from 'koa-compress'
 import cors from '@koa/cors'
 import mount from 'koa-mount'
+import send from 'koa-send'
 import serve from 'koa-static'
 
 import api from './api'
 import proxy from './proxy'
+
+// Public url to serve SPA stuff from
+const publicPath = process.env.PUBLIC_PATH || 'public'
 
 const app = new Koa()
 
@@ -20,11 +24,16 @@ app.use(cors())
 
 // Serve the main public assets
 // TODO: Would be nice to be able to hit a url without the /#/
-app.use(serve(process.env.PUBLIC_PATH || 'public'))
+app.use(serve(publicPath))
 
 // Mount the fancy stuff
 app.use(mount('/api', api))
 app.use(mount('/proxy/fflogs', proxy))
+
+// All other urls should serve up the index file, routing is handled by the SPA
+app.use(async ctx => {
+	await send(ctx, 'index.html', {root: publicPath})
+})
 
 // Boot the server
 app.listen(process.env.PORT || 3001)
