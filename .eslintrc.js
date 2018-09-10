fd45e0d01315a8ea5caaeb47b211a24dee86f69c
Saxon Landers <saxon@ackwell.com.au>
2018-09-10 22:54:03 +1000

    Update to shared eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,8 @@
 module.exports = {
-    env: {
-        es6: true,
-        node: true
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-        ecmaVersion: 2017,
-        sourceType: 'module'
-    },
-    rules: {
-        'indent': ['error', 'tab'],
-        'linebreak-style': ['error', 'unix'],
-        'quotes': ['error', 'single'],
-        'semi': ['error', 'never'],
-        'no-console': 'off'
-    }
+	extends: [
+		'plugin:xivanalysis/recommended'
+	],
+	env: {
+		node: true
+	},
 };
