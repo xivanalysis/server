e9383473f48ffe7afcf5f8516faa01da31188000
Saxon Landers <saxon@ackwell.com.au>
2019-01-26 16:46:38 +1100

    Add support for multiple branch builds on subdomains

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import '@babel/polyfill'
 import 'dotenv/config'
+import fs from 'fs'
 import Koa from 'koa'
+import path from 'path'
 import Raven from 'raven'
 
 import compress from 'koa-compress'
@@ -22,6 +24,7 @@ if (process.env.NODE_ENV === 'production' && process.env.RAVEN_DSN) {
 
 // Public url to serve SPA stuff from
 const publicPath = process.env.PUBLIC_PATH || 'public'
+const defaultClientBranch = process.env.DEFAULT_CLIENT_BRANCH || 'master'
 
 const app = new Koa()
 
@@ -31,8 +34,38 @@ app.use(compress())
 // Loosen up CORS a bit
 app.use(cors())
 
+// Work out the final public path
+async function canRead(path) {
+	return new Promise(resolve => {
+		fs.access(path, fs.constants.R_OK, err => resolve(!err))
+	})
+}
+app.use(async (ctx, next) => {
+	// Default to the legacy everything-in-top-level structure
+	let assetPath = publicPath
+
+	if (ctx.subdomains.length) {
+		// If there's subdomains, use the foremost (which is last in the array :eyes:)
+		const subdomain = ctx.subdomains[ctx.subdomains.length - 1]
+		assetPath = path.join(publicPath, subdomain)
+		// If we can't find the files for it, kill the request
+		if (!(await canRead(assetPath))) {
+			return
+		}
+	} else {
+		// Otherwise, try to use the default client branch
+		const newPath = path.join(publicPath, defaultClientBranch)
+		if (await canRead(newPath)) {
+			assetPath = newPath
+		}
+	}
+
+	ctx.state.assetPath = assetPath
+	await next()
+})
+
 // Serve the main public assets
-app.use(serve(publicPath))
+app.use(async (ctx, ...rest) => serve(ctx.state.assetPath)(ctx, ...rest))
 
 // Mount the fancy stuff
 app.use(mount('/api', api))
@@ -40,11 +73,11 @@ app.use(mount('/proxy/fflogs', proxy))
 
 // All other urls should serve up the index file, routing is handled by the SPA
 app.use(async ctx => {
-	// Don't do the fallback for files in /static, though - that just causes syntax errors when things go wrong
-	if (ctx.url.startsWith('/static')) {
+	// Don't do the fallback for files in /assets, though - that just causes syntax errors when things go wrong
+	if (ctx.url.startsWith('/assets')) {
 		return
 	}
-	await send(ctx, 'index.html', {root: publicPath})
+	await send(ctx, 'index.html', {root: ctx.state.assetPath})
 })
 
 // Boot the server
