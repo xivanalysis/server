04e4c3eac441bf206dbd5ca1575a80f797af6dc2
Saxon Landers <saxon@ackwell.com.au>
2019-08-07 12:28:39 +0000

    Fix edge case error on large AE filters

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps : [{
     name: 'xivanalysis',
     script: 'build/index.js',
+    node_args: '--max-http-header-size=20000',
     env: {
       NODE_ENV: 'development'
     },
