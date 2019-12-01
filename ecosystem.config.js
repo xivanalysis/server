dc56ee1bf7d9d0d1718547be4c342c4008e71c0d
Saxon Landers <saxon@ackwell.com.au>
2019-12-01 13:01:03 +1100

    Add rudimentary build steps for pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,18 @@
 module.exports = {
-  apps : [{
-    name: 'xivanalysis',
-    script: 'build/index.js',
-    node_args: '--max-http-header-size=20000',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production : {
-      NODE_ENV: 'production'
-    }
-  }],
-};
+	apps: [{
+		name: 'xivanalysis',
+		script: 'build/index.js',
+		node_args: '--max-http-header-size=20000',
+		env: {
+			NODE_ENV: 'development',
+		},
+		env_production: {
+			NODE_ENV: 'production',
+		},
+		post_update: [
+			'echo Building...',
+			'yarn install',
+			'yarn build',
+		],
+	}],
+}
