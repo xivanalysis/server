8e83c7fb683f7faf817b5d3b6fcd2bd7b80b6041
ackwell <saxon@ackwell.com.au>
2024-08-21 02:38:50 +1000

    Set up axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-	println!("Hello, world!");
+use axum::{debug_handler, response::IntoResponse, routing::get, serve, Router};
+use tokio::{net::TcpListener, signal};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+	let router = Router::new().route("/", get(root));
+
+	let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+	serve(listener, router)
+		.with_graceful_shutdown(shutdown_signal())
+		.await
+		.unwrap();
+
+	Ok(())
+}
+
+async fn shutdown_signal() {
+	let ctrl_c = async {
+		signal::ctrl_c()
+			.await
+			.expect("Failed to install Ctrl+C handler.");
+	};
+
+	#[cfg(unix)]
+	let terminate = async {
+		signal::unix::signal(signal::unix::SignalKind::terminate())
+			.expect("Failed to install SIGTERM handler.")
+			.recv()
+			.await;
+	};
+
+	#[cfg(not(unix))]
+	let terminate = std::future::pending::<()>();
+
+	tokio::select! {
+		_ = ctrl_c => {},
+		_ = terminate => {},
+	}
+}
+
+#[debug_handler]
+async fn root() -> impl IntoResponse {
+	"scaffold"
 }
