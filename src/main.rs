0554580195c784c823aadd0b38a818cc67b5884f
ackwell <saxon@ackwell.com.au>
2024-08-21 12:00:31 +1000

    Tracing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use axum::{debug_handler, response::IntoResponse, routing::get, serve, Router};
 use tokio::{net::TcpListener, signal};
+use tower_http::trace::TraceLayer;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{filter, layer::SubscriberExt, util::SubscriberInitExt, Layer};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-	let router = Router::new().route("/", get(root));
+	let filter = filter::Targets::new().with_default(LevelFilter::DEBUG);
+	let layer = tracing_subscriber::fmt::layer().with_filter(filter);
+	tracing_subscriber::registry().with(layer).init();
+
+	let router = Router::new()
+		.route("/", get(root))
+		.layer(TraceLayer::new_for_http());
 
 	let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
 	serve(listener, router)
@@ -36,6 +45,8 @@ async fn shutdown_signal() {
 		_ = ctrl_c => {},
 		_ = terminate => {},
 	}
+
+	tracing::info!("shutdown signal received")
 }
 
 #[debug_handler]
